59a7034c779b86bbc0cf5fc630299205f3517b11
Sadid Rafsun Tulon <srtulon6@gmail.com>
2019-03-25 00:25:05 +0600

    Update weather_station.ino

--- weather_station/weather_station.ino
+++ weather_station/weather_station.ino
@@ -1,7 +1,9 @@
+//created by srtulon
+
 #include <SoftwareSerial.h>
 #include <String.h>
  
-SoftwareSerial mySerial(6,7);
+SoftwareSerial mySerial(6,7); // RX, TX
 
 #include "U8glib.h"
 #include <Wire.h>
@@ -12,7 +14,10 @@ SoftwareSerial mySerial(6,7);
 
 #define DHTPIN  2        // Pin which is connected to the DHT sensor.
 
-int nRainDigitalIn = 3;
+#define nRainDigitalIn  3   // Pin which is connected to the Rain sensor.
+
+#define gmsPower 5    // Pin which is used to turn on SIM900
+
 boolean bIsRaining = false;
 String strRaining;
 int rain=0;
@@ -29,8 +34,6 @@ float humidity=0.0;
 #define DHTTYPE           DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE           DHT21     // DHT 21 (AM2301)
 
-// See guide for details on sensor wiring and usage:
-//   https://learn.adafruit.com/dht/overview
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -43,29 +46,31 @@ void setup()
   Serial.begin(9600);    // the GPRS baud rate
    dht.begin();
    pinMode(nRainDigitalIn,INPUT);
+   pinMode(gmsPower, OUTPUT);
+   digitalWrite(gmsPower,HIGH);
    sensor_t sensor;
   if (!bmp.begin()) {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   }
-    mySerial.println("AT");
+    mySerial.println(F("AT"));
   delay(1000);
 
-  mySerial.println("AT+CPIN?");
+  mySerial.println(F("AT+CPIN?"));
   delay(1000);
 
-  mySerial.println("AT+CREG?");
+  mySerial.println(F("AT+CREG?"));
   delay(1000);
 
-  mySerial.println("AT+CGATT?");
+  mySerial.println(F("AT+CGATT?"));
   delay(1000);
 
-  mySerial.println("AT+CIPSHUT");
+  mySerial.println(F("AT+CIPSHUT"));
   delay(1000);
 
-  mySerial.println("AT+CIPSTATUS");
+  mySerial.println(F("AT+CIPSTATUS"));
   delay(2000);
 
-  mySerial.println("AT+CIPMUX=0");
+  mySerial.println(F("AT+CIPMUX=0"));
   delay(2000);
  
   ShowSerialData();
@@ -73,7 +78,7 @@ void setup()
  
 void loop()
 {
-
+  digitalWrite(gmsPower,HIGH);
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   temp=event.temperature;
@@ -91,58 +96,58 @@ void Send2Pachube()
 {
 
  
-  mySerial.println("AT+CSTT=\"gpinternet\"");//start task and setting the APN,
+  mySerial.println(F("AT+CSTT=\"internet\""));// setting the APN , replace internet with proper APN
   updatedisplay();
   delay(700);
  
   ShowSerialData();
  
-  mySerial.println("AT+CIICR");//bring up wireless connection
+  mySerial.println(F("AT+CIICR"));//bring up wireless connection
   updatedisplay();
-  delay(2700);
+  delay(1000);
  
   ShowSerialData();
  
-  mySerial.println("AT+CIFSR");//get local IP adress
+  mySerial.println(F("AT+CIFSR"));//get local IP adress
   updatedisplay();
-  delay(1700);
+  delay(1000);
  
   ShowSerialData();
  
-  mySerial.println("AT+CIPSPRT=0");
+  mySerial.println(F("AT+CIPSPRT=0"));
   updatedisplay();
-  delay(2700);
+  delay(2000);
  
   ShowSerialData();
  
-  mySerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");//start up the connection
+  mySerial.println(F("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\""));//start up the connection
   updatedisplay();
-  delay(4700);
+  delay(2000);
  
   ShowSerialData();
  
-  mySerial.println("AT+CIPSEND");//begin send data to remote server
+  mySerial.println(F("AT+CIPSEND"));//begin send data to remote server
   updatedisplay();
-  delay(3700);
+  delay(2000);
   ShowSerialData();
  
-  String str1="GET http://api.thingspeak.com/update?api_key=BBXV6RSJMBXLEBHK&field1=" + String(temp)+"&field2="+String(humidity)+"&field3="+String(pre)+"&field4="+String(rain);
+  String str="GET http://api.thingspeak.com/update?api_key=xxxxxxxxxxxxx&field1=" + String(temp)+"&field2="+String(humidity)+"&field3="+String(pre)+"&field4="+String(rain);  // replace xxxxxxxxxxxxx with API  
    
-  //String str2="GET http://api.thingspeak.com/update?api_key=KO67GCLMXT31KE7J&field2=" + String(humidity);
-  mySerial.println(str1);//begin send data to remote server
+  
+  mySerial.println(str);//begin send data to remote server
   updatedisplay();
-  delay(3700);
+  delay(1000);
   ShowSerialData();
   
 
   mySerial.println((char)26);//sending
   updatedisplay();
-  delay(3700);//waitting for reply, important! the time is base on the condition of internet
+  delay(3700);     //waitting for reply
   mySerial.println();
  
   ShowSerialData();
  
-  mySerial.println("AT+CIPSHUT");//close the connection
+  mySerial.println(F("AT+CIPSHUT"));//close the connection
   updatedisplay();
   delay(100);
   ShowSerialData();
@@ -154,29 +159,24 @@ void ShowSerialData()
 }
 
 void draw(void) {
-  
-  //u8g.setFont(u8g_font_unifont);  // select font
-   u8g.setFont(u8g_font_6x12);
+    
+   u8g.setFont(u8g_font_6x12); // select font
   u8g.drawStr(0, 32, "T: ");  // put string of display at position X, Y
   u8g.drawStr(0, 52, "H: ");
-  u8g.drawStr(57, 32, "P: ");  // put string of display at position X, Y
+  u8g.drawStr(57, 32, "P: ");  
   u8g.drawStr(57, 52, "R: ");
   u8g.setPrintPos(20, 32);  // set position
-  u8g.print(temp, 0);  // display temperature from DHT11
+  u8g.print(temp, 0);  // display temperature from DHT22
   u8g.drawStr(33, 30, "c ");
   u8g.setPrintPos(20, 52);
-  u8g.print(humidity, 0);  // display humidity from DHT11
+  u8g.print(humidity, 0);  // display humidity from DHT22
   u8g.drawStr(33, 52, "% ");
   u8g.drawStr(110, 32, "Pa");
-  u8g.setPrintPos(70, 32);  // set position
+  u8g.setPrintPos(70, 32);  
   pre=String(bmp.readPressure());
-  Serial.println(bmp.readPressure());
-  u8g.print(pre);  // display temperature from DHT11
-  //u8g.drawStr(77, 32, pre);
-  //u8g.drawStr(110, 32, "Pa");
-  //u8g.drawStr(80, 52, strRaining);
+  u8g.print(pre);  // display air pressure from BMP180
   u8g.setPrintPos(80, 52);
-  u8g.print(strRaining);
+  u8g.print(strRaining); // display rain status
 }
 
 void updatedisplay(){
@@ -195,7 +195,7 @@ void updatedisplay(){
     rain=0;
   }
   u8g.firstPage(); 
-  Serial.println(String(temp)+"*C\t"+String(humidity)+"%");
+  
       delay(2);
       u8g.firstPage();  
   do {
